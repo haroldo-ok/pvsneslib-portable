5183511aa07c373564843ab511c83e401b275703
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-10-17 19:54:01 -0300

    Fix path to MSYS2Portable

--- env.bat
+++ env.bat
@@ -1,6 +1,7 @@
 set PVSNESLIB_DRIVE=%~d0
 set PVSNESLIB_PATH=%~p0
+set PVSNESLIB_RAW_HOME=%~dp0
 
 @FOR /F "delims=" %%s IN ('powershell -command "$env:PVSNESLIB_DRIVE.ToLower()"') DO @set PVSNESLIB_DRIVE=%%s
 set PVSNESLIB_HOME=/%PVSNESLIB_DRIVE::=%%PVSNESLIB_PATH:\=/%pvsneslib_430_64b_windows_release/pvsneslib
-path %PATH%;D:\Util\MSYS2Portable\App\msys64\usr\bin
+path %PATH%;%PVSNESLIB_RAW_HOME%MSYS2Portable\App\msys64\usr\bin
