8d725a302c6dc3352e2fc950096a1b999b1c432a
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-10-17 19:24:54 -0300

    Generate `PVSNESLIB_HOME` automatically

--- env.bat
+++ env.bat
@@ -1,2 +1,6 @@
-set PVSNESLIB_HOME=/d/Pessoal/Projetos/pvsneslib_430_64b_windows_release/pvsneslib
+set PVSNESLIB_DRIVE=%~d0
+set PVSNESLIB_PATH=%~p0
+
+@FOR /F "delims=" %%s IN ('powershell -command "$env:PVSNESLIB_DRIVE.ToLower()"') DO @set PVSNESLIB_DRIVE=%%s
+set PVSNESLIB_HOME=/%PVSNESLIB_DRIVE::=%%PVSNESLIB_PATH:\=/%pvsneslib_430_64b_windows_release/pvsneslib
 path %PATH%;D:\Util\MSYS2Portable\App\msys64\usr\bin
